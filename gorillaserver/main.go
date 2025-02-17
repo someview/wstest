5fa89a90e99839d43ba9099dafabb2ddbe983300
someview <fengyeall111@gmail.com>
2025-02-17 18:15:47 +0800

    feat:添加测试

--- gorillaserver/main.go
+++ gorillaserver/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Error during connection upgrade:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error during message reading:", err)
+			break
+		}
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			log.Println("Error during message writing:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handler)
+	log.Println("Server started at ws://localhost:8080/ws")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
